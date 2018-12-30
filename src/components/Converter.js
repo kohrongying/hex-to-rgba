001b0470d34b3b577a1871c347aa9c010b633993
Rong Ying <rongying@sgag.sg>
2018-12-30 14:23:45 +0800

    Refactor code with helpers.

--- src/components/Converter.js
+++ src/components/Converter.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import Switch from './Switch';
 import Shades from './Shades';
+import { hex2rgba, checkValidHex, rgba2hex, checkValidRGBA, checkDarkMode } from '../helpers';
 
 const Wrapper = styled.section`
   width: 100vw;
@@ -10,7 +11,6 @@ const Wrapper = styled.section`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  // padding: 4em;
   background: ${props => props.bg}
 `;
 
@@ -67,85 +67,26 @@ class Converter extends Component {
   }
 
   handleHexChange = (e) => {
-    if (this.checkValidHex(e.target.value)) {
-      const rgba = this.hex2rgba(e.target.value)
+    if (checkValidHex(e.target.value)) {
+      const rgba = hex2rgba(e.target.value)
       this.setState({
         bg: e.target.value,
         rgba: rgba,
-        darkMode: this.checkDarkMode(rgba)
+        darkMode: checkDarkMode(rgba)
       })
     }     
     this.setState({ hex: e.target.value })
   }
 
-  checkValidHex = (hex) => {
-    const regex = /^#[a-fA-F0-9]{6}$/
-    const regex2 = /^#[a-fA-F0-9]{8}$/
-    return regex.test(hex) || regex2.test(hex)
-  }
-
-  hex2rgba = (hex) => {
-    const [rr, gg, bb, aa] = hex.match(/\w\w/g)
-    const r = parseInt(rr, 16)
-    const g = parseInt(gg, 16)
-    const b = parseInt(bb, 16)
-    const alpha = aa ? (parseInt(aa, 16) / 255).toFixed(2) : 1.0
-    return `rgba(${r},${g},${b},${alpha})`
-  }
-
   handleRGBAChange = (e) => {
-    if (this.checkValidRGBA(e.target.value)) {
+    if (checkValidRGBA(e.target.value)) {
       this.setState({
         bg: e.target.value,
-        hex: this.rgba2hex(e.target.value),
-        darkMode: this.checkDarkMode(e.target.value)
+        hex: rgba2hex(e.target.value),
+        darkMode: checkDarkMode(e.target.value)
       })
     }
     this.setState({ rgba: e.target.value })
-  }
-
-  checkValidRGBA = (rgba) => {
-    let r = 0; 
-    let g = 0; 
-    let b = 0;
-    let a = 0;
-    if (rgba.slice(0,4) === "rgba") {
-      const [r, g, b, a] = rgba.slice(5,-1).split(',')
-    } else {
-      const [r, g, b] = rgba.slice(4,-1).split(',')
-      a = 1
-    }
-    return (
-      this.inRange(r, 255) && 
-      this.inRange(g, 255) && 
-      this.inRange(b, 255) &&
-      this.inRange(a, 1)
-    )
-  }
-
-  inRange = (n, upperLimit) => {
-    return parseInt(n) >= 0 && parseInt(n) <= upperLimit
-  }
-
-  rgba2hex = (rgba) => {
-    if (rgba.slice(0,4) === "rgba") {
-      const [r, g, b, a] = rgba.slice(5,-1).split(',')
-      const rr = parseInt(r).toString(16).padStart(2, '0')
-      const gg = parseInt(g).toString(16).padStart(2, '0')
-      const bb = parseInt(b).toString(16).padStart(2, '0')
-      const aa = Math.round((parseFloat(a) * 255)).toString(16).padStart(2, '0')
-      return `#${rr}${gg}${bb}${aa}`
-    } else {
-        const [r, g, b] = rgba.slice(4,-1).split(',')
-                              .map(i => parseInt(i).toString(16).padStart(2, '0'))
-        return `#${r}${g}${b}`                      
-    }
-  }
-
-  checkDarkMode = (rgba) => {
-    const [r, g, b, a] = rgba.slice(5,-1).split(',')
-    const val = parseInt(r) * 299 + parseInt(g) * 587 + parseInt(b) * 114 / 1000
-    return val < 123 ? true : false
   }
 
   toggleCopyMode = (e) => {
--- src/components/Palette.js
+++ src/components/Palette.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Colors } from '../colors';
+import { hex2rgba, checkValidHex, rgba2hex, checkValidRGBA, checkDarkMode } from '../helpers';
 
 const Wrapper = styled.section`
   width: 100vw;
@@ -26,6 +27,7 @@ const Input = styled.input`
     outline: none;
   }
 `;
+
 const HexInput = Input 
 const RGBAInput = Input
 
@@ -50,16 +52,12 @@ const PaletteItem = styled.div`
 `;
 
 class Palette extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      hex: '#ffffffff',
-      rgba: 'rgba(255,255,255,1.0)',
-      bg: '#ffffffff',
-      darkMode: false,
-      isHexCopied: true,
-      colors: []
-    }
+  state = {
+    hex: '#ffffffff',
+    rgba: 'rgba(255,255,255,1.0)',
+    bg: '#ffffffff',
+    darkMode: false,
+    colors: []
   }
   
   componentDidMount(){
@@ -67,104 +65,39 @@ class Palette extends Component {
     this.setState({ 
       colors: Colors[color],
       hex: Colors[color][4],
-      rgba: this.hex2rgba(Colors[color][4]),
+      rgba: hex2rgba(Colors[color][4]),
       bg: Colors[color][4]
     })
   }
 
   handleHexChange = (e) => {
-    if (this.checkValidHex(e.target.value)) {
-      const rgba = this.hex2rgba(e.target.value)
+    if (checkValidHex(e.target.value)) {
+      const rgba = hex2rgba(e.target.value)
       this.setState({
         bg: e.target.value,
         rgba: rgba,
-        darkMode: this.checkDarkMode(rgba)
+        darkMode: checkDarkMode(rgba)
       })
     }     
     this.setState({ hex: e.target.value })
   }
 
-  checkValidHex = (hex) => {
-    const regex = /^#[a-fA-F0-9]{6}$/
-    const regex2 = /^#[a-fA-F0-9]{8}$/
-    return regex.test(hex) || regex2.test(hex)
-  }
-
-  hex2rgba = (hex) => {
-    const [rr, gg, bb, aa] = hex.match(/\w\w/g)
-    const r = parseInt(rr, 16)
-    const g = parseInt(gg, 16)
-    const b = parseInt(bb, 16)
-    const alpha = aa ? (parseInt(aa, 16) / 255).toFixed(2) : 1.0
-    return `rgba(${r},${g},${b},${alpha})`
-  }
-
   handleRGBAChange = (e) => {
-    if (this.checkValidRGBA(e.target.value)) {
+    if (checkValidRGBA(e.target.value)) {
       this.setState({
         bg: e.target.value,
-        hex: this.rgba2hex(e.target.value),
-        darkMode: this.checkDarkMode(e.target.value)
+        hex: rgba2hex(e.target.value),
+        darkMode:checkDarkMode(e.target.value)
       })
     }
     this.setState({ rgba: e.target.value })
-  }
-
-  checkValidRGBA = (rgba) => {
-    let r = 0; 
-    let g = 0; 
-    let b = 0;
-    let a = 0;
-    if (rgba.slice(0,4) === "rgba") {
-      const [r, g, b, a] = rgba.slice(5,-1).split(',')
-    } else {
-      const [r, g, b] = rgba.slice(4,-1).split(',')
-      a = 1
-    }
-    return (
-      this.inRange(r, 255) && 
-      this.inRange(g, 255) && 
-      this.inRange(b, 255) &&
-      this.inRange(a, 1)
-    )
-  }
-
-  inRange = (n, upperLimit) => {
-    return parseInt(n) >= 0 && parseInt(n) <= upperLimit
-  }
-
-  rgba2hex = (rgba) => {
-    if (rgba.slice(0,4) === "rgba") {
-      const [r, g, b, a] = rgba.slice(5,-1).split(',')
-      const rr = parseInt(r).toString(16).padStart(2, '0')
-      const gg = parseInt(g).toString(16).padStart(2, '0')
-      const bb = parseInt(b).toString(16).padStart(2, '0')
-      const aa = Math.round((parseFloat(a) * 255)).toString(16).padStart(2, '0')
-      return `#${rr}${gg}${bb}${aa}`
-    } else {
-        const [r, g, b] = rgba.slice(4,-1).split(',')
-                              .map(i => parseInt(i).toString(16).padStart(2, '0'))
-        return `#${r}${g}${b}`                      
-    }
-  }
-
-  checkDarkMode = (rgba) => {
-    const [r, g, b, a] = rgba.slice(5,-1).split(',')
-    const val = parseInt(r) * 299 + parseInt(g) * 587 + parseInt(b) * 114 / 1000
-    return val < 123 ? true : false
-  }
-
-  toggleCopyMode = (e) => {
-    this.setState(prevState => ({
-      isHexCopied: !prevState.isHexCopied
-    }))
   }
 
   onClickItem = item => {
     this.setState({ 
       hex: item.target.textContent,
       bg: item.target.textContent,
-      rgba: this.hex2rgba(item.target.textContent)
+      rgba: hex2rgba(item.target.textContent)
     })
   }
 
